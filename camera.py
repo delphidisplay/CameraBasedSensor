2eccffbc51e9d31169e8adfb9a45a3f599ad6b17
chaitu65c <chaitu65c@gmail.com>
2020-04-27 18:55:13 -0700

    Rewrote Backend Code

--- camera.py
+++ camera.py
@@ -1,108 +1,93 @@
 # Python-specific imports
-import pickle
 from imutils.video import VideoStream
 
-# Package-specific imports
-import config
-from stream import stream_thread
+class Camera:
 
-"""									MODULE OVERVIEW
-	Contains all of the functions needed to manage and modify a camera.
-"""
+	""" 
+		url: The camera url passed in
+		ROI: A list containing all of the coordinates of the Bounding Box.
+		VS: A VideoStream object that streams from the camera url
+		dimensions: A list containing the width and height of each frame we would receive.
 
-
-def create_new_camera_specs(create=False):
 	"""
-		Creates a toy dictionary to be saved into camera_specs to allow fast functionality testing on the frontend.
-		For testing purposes only.
-	"""
-	if not create: # don't create a toy dictionary (use if you want to use already stored dictionary in camera_specs)
-		return
 
-	camera_dict = {"1": {"url": 0, "height": 720, "width": 1280,
-					  "ROI": [[1, 1], [2, 3], [5, 5], [6, 6], [1, 1]]},
-		   		"2": {"url": 0, "height": 720, "width": 1280,
-					  "ROI": [[10, 10], [20, 30], [50, 20], [60, 100], [10, 10]]}
-		   }
+	def __init__(self, url):
+		"""
+			Basic setup of the object, instanstiates url and starts a new video stream 
+		"""
+		self.url = url
+		self.ROI = None
+		self.initialize_video_stream(url)
 
-	# the format of the dictionary to be stored into camera_specs is as follows:
-	# the camera name is the key and the value is another dictionary with the following keys:
-	# url (specifies the stream path to connect to the physical camera)
-	# height (specifies the height of the camera stream frame)
-	# width (specifies the width of the camera stream frame)
-	# ROI (specifies the coordinates of the Region Of Interest created by the user on the frontend)
-
-	save_all_camera_specs(camera_dict) # saves the camera_dict to camera_specs
-
-def get_camera_specs():
-	"""
-		Retrieves the camera specification dictionary saved in camera_specs. See create_new_camera_specs() for
-		details on dictionary configuration.
-	"""
+	def __iter__(self):
+		""" 
+			Overwrites the default iter function so that you can iterate through this camera. 
+		"""
+		return CameraIterator(self)
 	
-	# Retrieves the pickle file and loads the pickle into the camera dictionary
-	with open('pickle_objects/camera_specs', "rb") as pickle_file: 
-		camera_dict = pickle.load(pickle_file)
-	return camera_dict
+	def __repr__(self):
+		""" 
+			String representation of the object. 
+		"""
+		return 'URL: {}, ROI: {}'.format(self.url,self.ROI)
 
-def save_camera_spec(camera_name, key, coordinates):
+	def set_roi_coordinates(self, coordinates):
+		""" 
+			Updates Region of Interest(ROI) with the coordinates specified from the frontend. 
+		"""
+		self.ROI = coordinates
+		
+	def initialize_video_stream(self,camera_url):
+		""" 
+			Given a camera url, build a stream object and get the dimensions of it. 
+		"""
+		# Build Stream
+		self.VS = VideoStream(src=camera_url).start()
+		
+		# If we are not able to read a proper frame from the stream, this will fail.
+		sample_frame = self.VS.read()
+		assert sample_frame is not None
+
+		# Set the width and height.
+		self.dimensions = sample_frame.shape		
+
+	def stop_video_stream(self):
+		""" 
+			Turns off the Video Stream 
+		"""
+		self.VS.stop()
+		
+class CameraIterator:
+
+	""" 
+		This object is created so that you can iterate through a camera object 
 	"""
-		Updates the camera in the camera dictionary with a new ROI with the given coordinates.
-	"""
 
-	# Loads up the camera dictionary
-	camera_dict = get_camera_specs()
+	def __init__(self, camera):
+		""" 
+			Basic setup of iterator object. 
+		"""
+		self.camera = camera
 
-	camera_dict[camera_name][key] = value # save key, value pair for specific camera_name
+	def __next__(self):
+		""" 
+			Allows iterating over this object to get each frame. Ex: "for frame in camera..." 
+		"""
 
-	# Stores the modified dictionary into the camera dictionary
-	save_all_camera_specs(camera_dict) # save camera_dict into camera_specs
+		# If we are not able to read a proper frame from the stream, this will fail.
+		frame = self.camera.VS.read()
+		assert frame is not None
 
-def save_all_camera_specs(camera_dict):
-	"""
-		Stores the camera dictionary as a pickle object which is named pickle_objects/camera_specs
-	"""
+		return frame
 
-	# Retrives the file and saves the camera dictionary into the file
-	with open('pickle_objects/camera_specs', "wb") as pickle_file:
-		pickle.dump(camera_dict, pickle_file)
-
-def valid_camera_url(camera_url):
-	"""
-		Takes in a camera url as input and checks if the camera url produces a valid video stream. Returns the validity,
-		height, and width of the video stream specified by the camera url.
-	"""
-
-	# Attempts to connect to the steram specified by the camera_url
-	vs = VideoStream(src=camera_url).start()
- 
-	# Reads a single frame so that we can test the validity of the stream
-	vs_frame = vs.read()
-
-	# Stops the stream. 
-	vs.stop()
-
-	# If the stream has issues, then we would not have a frame stored in vs_frame.
-	is_valid = True if vs_frame is not None else False 
-	width, height = (vs_frame.shape[0], vs_frame.shape[1]) if vs_frame is not None else (0,0) # get stream dimensions
-
-	return is_valid, width, height
-
-def set_next_available_camera():
-	"""
-		Sets the current camera to the next available camera for streaming on the frontend. This function is invoked when
-		the user removes the camera that is currently streaming to the frontend.
-	"""
-	camera_dict = get_camera_specs()
-
-	cameras_available = list(camera_dict.keys()) # get names of camera's available
-	if len(cameras_available) != 0:
-		new_camera_name = list(camera_dict.keys())[0] # choose first camera in the list to stream
-		new_camera_url = camera_dict[new_camera_name]["url"] # get the camera url of new_camera_name
-
-		config.CURRENT_CAMERA = new_camera_name # set the current camera
-		stream_thread(new_camera_name, new_camera_url) # start streaming from the chosen camera
-
-	else:  # if no cameras available, handle here
-		config.CURRENT_CAMERA = None # no camera is available, CURRENTLY PRODUCES ERROR
-		config.CAMERA_THREAD.join() # kills the current thread used to stream
+""" 
+	Testing to check for how long it takes to run 
+"""
+if __name__ == '__main__':
+	sample = Camera(url = 0)
+	i = 0
+	for frame in sample:
+		if i > 1:
+			break
+		print(frame)
+		i += 1
--- main.py
+++ main.py
@@ -1,52 +1,51 @@
-# Flask imports
+# Python-specific imports
 from flask import Flask, request, render_template, Response, flash
+from datetime import datetime
+import cv2
 
-# Import Global Variables
-import config # global variables
-
-# Import helper functions from other files
-from stream import stream_camera_frame, generate, stream_thread
-from camera import create_new_camera_specs, get_camera_specs, save_camera_spec, \
-	save_all_camera_specs, valid_camera_url, set_next_available_camera
-from roi import is_valid_roi
+# Package-specific imports
+from camera import Camera 
+from frontend_utils import *
 
 # Main Flask used for routing.
 app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
+camera_dictionary = {}
+current_camera = 0
+camera_dictionary[0] = Camera(0)
+
+def __get_frames():
+	"""
+		Generator function to get frames constantly to the frontend.
+	"""
+	for frame in camera_dictionary[current_camera]:
+		yield(prepare_frame_for_display(frame, current_camera))
+
 @app.route('/')
 def show_stream():
-	"""Function called when Flask boots up for the first time."""
-	create_new_camera_specs(create=True) # creates toy camera_specs object for testing purposes only
-	return render_template('show_stream.html', camera_dict=get_camera_specs(), current_camera=config.CURRENT_CAMERA)
+	"""
+		Function called when Flask boots up for the first time.
+	"""
+	return render_template('show_stream.html', camera_dict=camera_dictionary, current_camera=current_camera)
 
 @app.route("/stream_feed")
 def stream_feed():
 	"""
-	Frontend continuously calls this function to show the current stream. This returns the response generated
-	(current streaming frame) along with the specific media type that is understood by the frontend in order
-	to render the frame.
+		Utilizes the generator function main.py::__get_frames() to send frames from the current_camera stream into the frontend.
 	"""
-	return Response(generate(), mimetype = "multipart/x-mixed-replace; boundary=frame")
+	return Response(__get_frames(), mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 @app.route('/record_roi', methods=['POST'])
 def record_roi():
 	"""
-	When the user defines and submits a Region Of Interest (ROI) on the frontend this function is called.
-	The POST request retrieves a list of x,y ROI coordinates created from a JavaScript object stored in request.form
-	which must be parsed into a Python list of  [x,y] coordinates. If the coordinates are valid
-	ie. no edges of the polygon created intersect, then the ROI is saved for the respective camera in camera_specs.
-	Coordinates are saved as list of [x,y] instead of list of (x,y) because JavaScript allows easier manipulation
-	of lists. The ROI coordinates are used by JavaScript to render the ROI dynamically on the frontend.
-
-	IMPORTANT:
-	As of 4/26/2020 the ROI x,y coordinates are relative to the stream dimensions on the frontend.
-	This will need to be updated to have the ROI coordinates relative to the frame size of the input to the YOLO model
-	in order to have the ROI against the YOLO model's produced bounding boxes to determine if a vehicle is in the ROI.
+		Updates the current camera stream's ROI coordinates.
 	"""
+
+	# TODO: Make the x and y coordinates relative to the frame size of the input to the YOLO model.
+
 	print(request.form)
-	camera_name = config.CURRENT_CAMERA # get the camera the ROI coordinates submitted are meant to be associated with
 
 	roi_coord = []
 	for rc in range(len(request.form)//2): # translate the received ROI in request.form into a Python list of coordinates
@@ -56,83 +55,66 @@ def record_roi():
 	print(roi_coord)
 
 	if is_valid_roi(roi_coord): # validate the ROI coordinates
-		print("VALID ROI SPECIFIED")
-		save_camera_spec(camera_name, "ROI", roi_coord) # save the ROI for the specific camera_name to camera_specs
+		#print("VALID ROI SPECIFIED")
+		camera_dictionary[current_camera].set_roi_coordinates(roi_coord)
 	else:
 		print("INVALID ROI: MUST SPECIFY POLYGON")
 
-	return render_template('show_stream.html', camera_dict=get_camera_specs(), current_camera=config.CURRENT_CAMERA)
+	return render_template('show_stream.html', camera_dict=camera_dictionary, current_camera=current_camera)
 
 @app.route('/choose_camera', methods=['POST'])
 def choose_camera():
 	"""
-	When the user selects a camera stream to view on the frontend this function is called. This POST request retrieves
-	the camera name. This function sets the current camera from config to be the chosen camera and calls stream_thread
-	that kills the current stream and enables the stream from the chosen camera onto the frontend.
+		Switches the camera stream that's being displayed on the frontend.
 	"""
-	camera_name = request.form["camera_view"]
-	config.CURRENT_CAMERA = camera_name # set the current camera
+	current_camera = request.form["camera_view"]
 
-	camera_dict = get_camera_specs()
-	camera_url = camera_dict[camera_name]["url"] # get the current camera url
+	if current_camera == '0':
+		current_camera = 0
 
-	print("CHOOSE_CAMERA", camera_name)
-	stream_thread(camera_name, camera_url) # start streaming from the chosen camera
+	print("CURRENT CAMERA IS NOW {}".format(current_camera))
 
-	return render_template('show_stream.html', camera_dict=get_camera_specs(), current_camera=config.CURRENT_CAMERA)
+	return render_template('show_stream.html', camera_dict=camera_dictionary, current_camera=current_camera)
 
 @app.route('/add_camera', methods=['POST'])
 def add_camera():
 	"""
-	When the user creates a new camera on the frontend this function is called. The POST request retrieves
-	the camera name and camera url. If the camera url is validated to work, then the camera is saved in camera_specs.
+		Adds a new camera to the system with the user specified camera url.
 	"""
+
 	camera_name = request.form["camera_name"]
 	camera_url = request.form["stream_url"]
 
-	if camera_url == "0": # special command to enable streaming from computer webcam
+	# Special case which indicates the computer's webcam
+	if camera_url == "0": 
 		camera_url = 0
 
-	is_valid_camera_url, width, height = valid_camera_url(camera_url) # validate camera url and retrieve dimensions
-	if is_valid_camera_url:
-		camera_dict = get_camera_specs()
-
-		if camera_name not in camera_dict: # save camera to camera_specs if camera_name does not exist
-			camera_dict[camera_name]={"url":camera_url, "width": width, "height":height, "ROI": None}
-			# structure for camera_specs entry above; takes url, width, height, and ROI; camera_name is the key
-			save_all_camera_specs(camera_dict) # saves camera_dict as camera_specs
-		else:
-			print("INVALID ENTRY: CAMERA NAME TO ADD ALREADY IN USE")
+	if camera_name not in camera_dictionary:
+		camera_dictionary[camera_name] = Camera(camera_url)
 	else:
-		print("INVALID ENTRY: NO CONNECTION TO RTSP URL")
+		print("ERROR: CAMERA EXISTS")
 
-	return render_template('show_stream.html', camera_dict=get_camera_specs(), current_camera=config.CURRENT_CAMERA)
+	return render_template('show_stream.html', camera_dict=camera_dictionary, current_camera=current_camera)
 
 @app.route('/remove_camera', methods=['POST'])
 def remove_camera():
 	"""
-	When the user removes a camera from the frontend this function is called. The POST request retrieves the camera
-	name to be removed. The camera name is checked to be a valid camera currently in camera_specs before it is removed.
-	If the removed camera is the camera currently streaming to the frontend, then a new camera is chosen to be the
-	current streamed camera.
+		Removes a camera from the system and ends the camera's video stream.
 	"""
 	camera_name = request.form["remove_name"]
-	camera_dict = get_camera_specs()
 
-	if camera_name in camera_dict: # check if camera name exists
-		camera_dict.pop(camera_name, None)
-		save_all_camera_specs(camera_dict) # save camera_dict to camera_specs since camera is removed
+	if camera_name in camera_dictionary: 
+		camera_dictionary[camera_name].stop_video_stream()
+		del(camera_dictionary[camera_name])
 
-		if config.CURRENT_CAMERA == camera_name: # check if camera removed is the current camera being streamed
-			set_next_available_camera() # choose another camera to stream to the frontend
+		# If the camera being removed was the current camera, set a new camera stream to display onto the frontend
+		if camera_dictionary and current_camera == camera_name:
+			current_camera = next(camera_dictionary)
+
 	else:
 		print("INVALID ENTRY: CAMERA NAME TO REMOVE DOES NOT EXIST")
 
-	return render_template('show_stream.html', camera_dict=get_camera_specs(), current_camera=config.CURRENT_CAMERA)
+	return render_template('show_stream.html', camera_dict=camera_dictionary, current_camera=current_camera)
 
 if __name__ == "__main__":
-
 	app.run(host="0.0.0.0", debug=True)
-
-	if config.VS is not None:
-		config.VS.stop() # kill connection to current physical camera
