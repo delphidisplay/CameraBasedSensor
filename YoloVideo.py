853fb11379f6f7c5e234249adfe94226e680f394
Rohit Sriram <rohitsriram14@gmail.com>
2020-05-09 13:51:37 -0700

    Added a comment just to test committing

--- YoloVideo.py
+++ YoloVideo.py
@@ -4,15 +4,17 @@ import cv2
 from find_intersect import intersection_of_polygons
 import matplotlib.pyplot as plt
 
+#Added a comment just to get in the flow
+
 class YoloVideo:
 	"""
 	detection model to identify cars and trucks within a specific region of interest (ROI)
-	""" 
+	"""
 
 	def __init__(self, net):
 		"""
 			self.frame: frame from stream
-			self.ROI: nested list defining region of intereest in frame in which we detect vehicles 
+			self.ROI: nested list defining region of intereest in frame in which we detect vehicles
 			self.confidence: minimum probability to filter weak detections
 			self.threshold: threshold when applying non-maxima suppression
 		"""
@@ -32,7 +34,7 @@ class YoloVideo:
 			return the COCO class labels our YOLO model was trained on
 		"""
 		return open("yolo-coco/coco.names").read().strip().split("\n")
-		
+
 
 	def initiailize_colors(self, labels):
 		"""
@@ -52,10 +54,10 @@ class YoloVideo:
 	def detect_in_frame(self, output_time=False):
 		"""
 			detect vehicle in frame
-			returns layer outputs, which contains class id and confidence probabilities 
-		"""	
+			returns layer outputs, which contains class id and confidence probabilities
+		"""
 
-		#get yolo object and layer names 
+		#get yolo object and layer names
 		#self.net = self.get_yolo_object()
 		ln = self.get_layer_names()
 
@@ -66,7 +68,7 @@ class YoloVideo:
 		self.net.setInput(blob)
 		start = time.time()
 		layerOutputs = self.net.forward(ln)
-		end = time.time() 
+		end = time.time()
 		if output_time == True:
 			elap = (end - start)
 			print("[INFO] single frame took {:.4f} seconds".format(elap))
@@ -82,10 +84,10 @@ class YoloVideo:
 		confidences= []
 		classIDs = []
 		layerOutputs = self.detect_in_frame()
-		
+
 		#grab frame dimensions
 		(H,W) = self.frame.shape[:2]
-		
+
 		# loop over each of the layer outputs
 		for output in layerOutputs:
 			# loop over each of the detections
@@ -105,7 +107,7 @@ class YoloVideo:
 
 					# use the center (x, y)-coordinates to derive the top
 					# and and left corner of the bounding box
-					x = int(centerX - (width / 2))				
+					x = int(centerX - (width / 2))
 					y = int(centerY - (height / 2))
 
 					# update our list of bounding box coordinates,confidences and class IDs
@@ -120,7 +122,7 @@ class YoloVideo:
 		boxes = self.extract_detection_information()[0]
 		confidences = self.extract_detection_information()[1]
 		idxs = cv2.dnn.NMSBoxes(boxes, confidences, self.confidence, self.threshold)
-		return idxs 
+		return idxs
 
 	def detect_intersections(self):
 		"""
@@ -128,14 +130,14 @@ class YoloVideo:
 			self.net: yolo object
 		"""
 		idxs = self.apply_suppression()
-		LABELS = self.get_yolo_labels() 
+		LABELS = self.get_yolo_labels()
 		boxes = self.extract_detection_information()[0]
 		classIDs = self.extract_detection_information()[2]
 
 		#ensure at least one detection exists
 		if len(idxs) > 0:
 			#loop over indexes we are keeping
-			carAmount = 0 
+			carAmount = 0
 			for i in idxs.flatten():
 				#extract the bounding box coordinates
 				(x, y) = (boxes[i][0], boxes[i][1])
@@ -143,20 +145,20 @@ class YoloVideo:
 
 				#get shape of bounding box to get intersection with ROI
 				bounding_box = [(x,y),(x,y+h),(x+w,y+h),(x+w,y),(x,y)]
-				
+
 				if self.debug:
-					#plot bounding box and ROI to see if they make sense  
+					#plot bounding box and ROI to see if they make sense
 					bounding_box_x = [x,x,x+w,x+w,x]
 					bounding_box_y = [y,y+h,y+h,y,y]
-					ROI_x = [i[0] for i in self.ROI] 
-					ROI_y = [i[1] for i in self.ROI] 
+					ROI_x = [i[0] for i in self.ROI]
+					ROI_y = [i[1] for i in self.ROI]
 					print("bounding_box: " , bounding_box)
 					print("self.ROI: ", self.ROI)
 					#plt.plot(bounding_box_x,bounding_box_y, label="BBOX", linewidth=4, color="orange")
 					#plt.plot(ROI_x, ROI_y, label="ROI", linewidth=4, color="magenta")
 					plt.title("Figure {}, Intersect Threshold: {}, Vehicle Counted: {}".format("1", "0", True))
 					#plt.show()
-				
+
 				intersects_flag = intersection_of_polygons(self.ROI,bounding_box)
 				if intersects_flag:
 					if LABELS[classIDs[i]] == "car" or LABELS[classIDs[i] == "truck"]:
@@ -164,8 +166,8 @@ class YoloVideo:
 
 			#carsAmount = str(carAmount) + " vehicles in ROI"
 			return carAmount
-		return 0		
-			
+		return 0
+
 '''
 if __name__ == "__main__":
 	yolo_model = YoloVideo(cv2.imread("images/car.jpg"),[[116, 28], [115, 87], [204, 297], [431, 278], [503, 138], [481, 37], [295, 27], [117, 22], [116, 28]],"yolo-coco")
