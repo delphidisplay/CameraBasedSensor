838244962a1921656cbc97a765d47b818ab4bb78
Gautam Banuru <gbanuru@uci.edu>
2020-05-20 18:08:24 -0700

    modified comment spacing

--- camera.py
+++ camera.py
@@ -4,18 +4,18 @@ from imutils.video import VideoStream
 class Camera:
 
 	"""
-	url: The camera url passed in
-	ROI: A list containing all of the coordinates of the Bounding Box.
-	VS: A VideoStream object that streams from the camera url
-	dimensions: A list containing the width and height of each frame we would receive.
-	prepare_ratio: A list containing the ratio of the original frame to the frame that's resized when the frame is preparing for it to be displayed.
-	frontend_ratio: A list containing the ratio of the original frame to the frame that's resized when the frame is displayed on the frontend.
-	car_count: number of cars that have passed by this camera
+		url: The camera url passed in
+		ROI: A list containing all of the coordinates of the Bounding Box.
+		VS: A VideoStream object that streams from the camera url
+		dimensions: A list containing the width and height of each frame we would receive.
+		prepare_ratio: A list containing the ratio of the original frame to the frame that's resized when the frame is preparing for it to be displayed.
+		frontend_ratio: A list containing the ratio of the original frame to the frame that's resized when the frame is displayed on the frontend.
+		car_count: number of cars that have passed by this camera
 	"""
 
 	def __init__(self, url):
 		"""
-		Basic setup of the object, instanstiates url and starts a new video stream
+			Basic setup of the object, instanstiates url and starts a new video stream
 		"""
 		self.url = url
 		self.ROI = None
@@ -25,25 +25,25 @@ class Camera:
 
 	def __iter__(self):
 		"""
-		Overwrites the default iter function so that you can iterate through this camera.
+			Overwrites the default iter function so that you can iterate through this camera.
 		"""
 		return CameraIterator(self)
 
 	def __repr__(self):
 		"""
-		String representation of the object.
+			String representation of the object.
 		"""
 		return 'URL: {}, ROI: {}'.format(self.url,self.ROI)
 
 	def set_roi_coordinates(self, coordinates):
 		"""
-		Updates Region of Interest(ROI) with the coordinates specified from the frontend.
+			Updates Region of Interest(ROI) with the coordinates specified from the frontend.
 		"""
 		self.ROI = coordinates
 
 	def initialize_video_stream(self,camera_url):
 		"""
-		Given a camera url, build a stream object and get the dimensions of it.
+			Given a camera url, build a stream object and get the dimensions of it.
 		"""
 
 		# Build Stream
@@ -60,24 +60,24 @@ class Camera:
 
 	def stop_video_stream(self):
 		"""
-		Turns off the Video Stream
+			Turns off the Video Stream
 		"""
 		self.VS.stop()
 
 class CameraIterator:
 	"""
-	This object is created so that you can iterate through a camera object
+		This object is created so that you can iterate through a camera object
 	"""
 
 	def __init__(self, camera):
 		"""
-		Basic setup of iterator object.
+			Basic setup of iterator object.
 		"""
 		self.camera = camera
 
 	def __next__(self):
 		"""
-		Allows iterating over this object to get each frame. Ex: "for frame in camera..."
+			Allows iterating over this object to get each frame. Ex: "for frame in camera..."
 		"""
 
 		# If we are not able to read a proper frame from the stream, this will fail.
